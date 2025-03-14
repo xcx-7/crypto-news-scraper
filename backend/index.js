f0aa032036cfb0e6cf0f8e0bb4b5a9d01c29f623
xcx-7 <zainab.42847@gmail.com>
2025-03-14 19:16:24 +0600

    update

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,3 @@
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-
-// const url = 'https://www.coindesk.com/';
-
-// async function scrapeNews() {
-//     try {
-//         const { data } = await axios.get(url);
-        
-//         const $ = cheerio.load(data);
-
-//         const articles = [];
-        
-//         $('.font-headline-xs').each((index, element) => {
-//             const headline = $(element).text().trim();
-//             const link = $(element).attr('href');
-            
-//             articles.push({
-//                 headline,
-//                 link: `https://www.coindesk.com${link}`,
-//             });
-//         });
-
-//         console.log(articles);
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//     }
-// }
-
-// scrapeNews();
-
-
-
-
-
 const axios = require('axios');
 const cheerio = require('cheerio');
 
@@ -45,11 +10,9 @@ async function scrapeNews() {
 
         const articles = [];
 
-        // Target the .font-headline-xs elements
         $('.font-headline-xs').each((index, element) => {
             const headline = $(element).text().trim();
 
-            // Find the closest parent <a> tag or a sibling <a> tag
             const linkElement = $(element).closest('a');
             const link = linkElement.attr('href');
 
@@ -60,7 +23,7 @@ async function scrapeNews() {
                 });
             }
         });
-
+        
         console.log(articles);
     } catch (error) {
         console.error('Error fetching data:', error);
