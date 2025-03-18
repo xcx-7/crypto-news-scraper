dc17c94eb0b44c50d49f9925ff6298501c1aca83
xcx-7 <zainab.42847@gmail.com>
2025-03-18 12:07:47 +0600

    database added

--- backend/newsSchema.js
+++ backend/newsSchema.js
@@ -1,10 +1,12 @@
+const mongoose = require("mongoose");
+
 const newsSchema = new mongoose.Schema({
     headline: { type: String, required: true },
-    link: { type: String, required: true, unique: true }, // Ensure unique links
-    publishDate: String,
+    link: { type: String, required: true, unique: true }, 
     author: String,
     summary: String,
     thumbnail: String
 });
 
-export const News = mongoose.model("News", newsSchema);
+const News = mongoose.model("News", newsSchema);
+module.exports = News;
